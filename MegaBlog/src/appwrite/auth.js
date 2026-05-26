31a433b31581d5a4decfed709791e6f585d5a04d
Mahesh Baviskar <maheshbaviskar@Mac.lan>
2026-05-26 11:19:43 +0530

    MegaBlog setup and Appwrite set up done

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,66 @@
+import conf from "../config/config";
+import { Client, Account, ID } from "appwrite"
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    /* Creating account */
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+
+            if (userAccount) {
+                ///call another method
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+
+        }
+    }
+
+    /* Login function */
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /* Getting current user info */
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+
+    async logout() {
+        try {
+            await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default AuthService;
